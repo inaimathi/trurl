79fbe562843cf4a246cd5c0044834dd7348b7b97
inaimathi <leo.zovic@gmail.com>
2017-09-08 07:05:56 -0400

    Add gg machine, and first attempt at builder

--- machines.lisp
+++ machines.lisp
@@ -1,13 +1,25 @@
 (in-package #:trurl)
 
 (defparameter *machines*
-  '(line ray box wander bomb ff))
+  '(line ray box wander bomb ff gg))
 
 (lem:define-machine wander
   (let ((dest (pick (loop for (x y) in lem:n*extended
 		       for c = (lem:neighbor x y)
 		       when (and c (lem:empty? c)) collect c))))
     (when dest (lem:move-to! dest lem:here))))
+
+;; (lem:define-machine builder
+;;   (flet ((all-empty? ()
+;; 	   (every
+;; 	    (lambda (xy)
+;; 	      (if (equal '(0 0) xy)
+;; 		  t
+;; 		  (lem:empty? (lem:neighbor (first xy) (second xy)))))
+;; 	    lem:n*extended)))
+;;     (let ((dest (pick (loop for (x y) in lem:n*extended
+;; 			 for c = (lem:neighbor x y)
+;; 			 when (and c (lem:empty? c)) collect c)))))))
 
 (lem:define-machine bomb
   (loop for (x y) in lem:n*extended
@@ -17,3 +29,7 @@
   (loop for (x y) in lem:n*extended
      for c = (lem:neighbor x y)
      do (when (lem:empty? c) (lem:spawn-in! c lem:self))))
+
+(lem:define-machine gg
+    (loop for (x y) in lem:n*extended
+	 do (lem:spawn-in! (lem:neighbor x y) lem:self)))
